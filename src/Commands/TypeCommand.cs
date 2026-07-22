f127c02432bb80cad29bc3ac331d286cad876fdd
Cristobal Barreto <44922559+epeat96@users.noreply.github.com>
2026-07-22 10:47:19 -0300

    codecrafters submit [skip ci]

--- src/Commands/TypeCommand.cs
+++ src/Commands/TypeCommand.cs
@@ -4,21 +4,21 @@ public class TypeCommand : ICommand
 {
     public EvalCode Execute(string command, string[] args)
     {
-        var verifiedCommand = args.First();
-        var searchResult = CommandsHelper.GetCommand(verifiedCommand);
+        var typedCommand = args.First();
+        var searchResult = CommandsHelper.GetCommand(typedCommand);
         if (searchResult is null)
         {
-            var pathSearchResult = CommandsHelper.SearchPathForCommand(verifiedCommand, args);
+            var pathSearchResult = CommandsHelper.SearchPathForCommand(typedCommand, args);
             if (pathSearchResult is null)
             {
-                return CommandsHelper.CommandNotFound(verifiedCommand);
+                return CommandsHelper.CommandNotFound(typedCommand);
             }
-            Console.WriteLine($"{command} is {pathSearchResult}");
+            Console.WriteLine($"{typedCommand} is {pathSearchResult}");
 
             return EvalCode.Success;
         }
         
-        Console.WriteLine($"{verifiedCommand} is a shell builtin");
+        Console.WriteLine($"{typedCommand} is a shell builtin");
         return EvalCode.Success;
     }
 }
