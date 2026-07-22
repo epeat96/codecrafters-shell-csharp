e5839e2a217dcb6935c7bac02098cb6ce9aff91c
Cristobal Barreto <44922559+epeat96@users.noreply.github.com>
2026-07-21 23:56:06 -0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -7,17 +7,41 @@ class Program
         do
         {
             Console.Write("$ ");
-            command = Console.ReadLine() ?? "";
-        }while(Eval(command) != 0);
+            var userInput = Console.ReadLine()!.Split(' ');
+            command = userInput.First();
+            var args = userInput.Skip(1).ToArray();
+                
+        }while(!Eval(command,args).Equals(EvalCode.Success));
     }
 
-    static int Eval(string command)
+    enum EvalCode
     {
-        if(command.Equals("exit")) 
-        {
-            return 0;
-        }
+        Success = 0,
+        CleanExit = 1,
+        Error = 2
+    }
+
+    static EvalCode Eval(string command, string[] args) => command switch
+    {
+       "exit" => Exit(),
+       "echo" => Echo(args),
+        _ => NotFound(command),
+    };
+
+    static EvalCode Exit()
+    {
+        return EvalCode.Success;
+    }
+
+    static EvalCode Echo(string[] args)
+    {
+        var output = string.Join(" ", args);
+        Console.WriteLine(output);
+        return EvalCode.CleanExit;
+    }
+    static EvalCode  NotFound(string command)
+    {
         Console.WriteLine($"{command}: command not found");
-        return 1;
+        return EvalCode.Error;
     }
 }
